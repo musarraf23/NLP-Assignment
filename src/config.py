ed7e76092a0f014c917926d02fe7e3f0d2802fd5
prashanthkumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2025-11-23 00:54:50 +0530

    Fix(config): bug fixes

--- src/config.py
+++ src/config.py
@@ -76,7 +76,7 @@ class Dialogues:
 
 questions : list[Question] = [
     Question(question="How many times can you subtract the number 5 from 35?", answers=["seven", "7"]),
-    Question(question="Jack is looking at Anne. Anne is looking at George. Jack is married, George is not, and we don't know if Anne is married. Is a married person looking at an unmarried person?", answers={"yes", "yep", "yo", "yeah"}),
+    Question(question="Jack is looking at Anne. Anne is looking at George. Jack is married, George is not, and we don't know if Anne is married. Is a married person looking at an unmarried person?", answers=["yes", "yep", "yo", "yeah"]),
     Question(question="The day before two days after the day before tomorrow is Saturday. What day is it today?", answers=["friday"]),
     Question(question="Susan and Lisa decided to play tennis against each other. They bet $1 on each game they played. Susan won three bets and Lisa won $5. How many games did they play?", answers=["eleven"]),
 ]
